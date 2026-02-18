61db530d1d003f757634184276720f8430893ca7
AshbirSingh <ashbirsingh251@gmail.com>
2026-02-18 10:04:08 -0800

    LJK

---
+++ myFirstProgram.c
@@ -0,0 +1,11 @@
+
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)(
+    string name = "Asbir";
+   int age = "17;
+   float money = 2.32;
+   printf("%s is, %1 years old \n", name, age);
+   printf("%s has $%.2f in his wallet.\n",name, money);
+)
