1ad2b9420413560255809d4a4e48f4b495ae2ce0
AshbirSingh <ashbirsingh251@gmail.com>
2026-03-18 17:05:49 +0000

    Wed, Mar 18, 2026, 10:05 AM -07:00

--- script/insect.js
+++ script/insect.js
@@ -11,6 +11,6 @@ for (let i = 0; i < choose_insect_btns.length; i = i + 1)
 {
     choose)insect_btnsp[i].addEventListener('click', () => {
         screens[1].classList.add('up')
-        
+        img = choose_insect_btns[1].querySelector
     })
 }
