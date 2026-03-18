80494fe7ff5810f2a073d53f5ba0f26ec8a8f86b
AshbirSingh <ashbirsingh251@gmail.com>
2026-03-18 16:56:11 +0000

    Wed, Mar 18, 2026, 9:56 AM -07:00

--- script/insect.js
+++ script/insect.js
@@ -1,4 +1,4 @@
 screens = document.querySelectorALL('.screen')
 choose_insect_btns = document.querySelectorALL('.choose-insect-btn')
+start_btn = document.getElementById('start-btn')
 
-choose_insect_btns[0]
