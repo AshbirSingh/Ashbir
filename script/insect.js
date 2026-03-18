b2d8722b9e2762a4588e0e8f240a842b8f362c4c
AshbirSingh <ashbirsingh251@gmail.com>
2026-03-18 16:59:12 +0000

    Wed, Mar 18, 2026, 9:59 AM -07:00

--- script/insect.js
+++ script/insect.js
@@ -2,3 +2,7 @@ screens = document.querySelectorALL('.screen')
 choose_insect_btns = document.querySelectorALL('.choose-insect-btn')
 start_btn = document.getElementById('start-btn')
 
+
+start_btn.addEventListener('click', () => {
+    screens[0]. classlist.add('up')
+})
