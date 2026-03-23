1faacd92bb7e7688985adc1872e1499334563e3b
AshbirSingh <ashbirsingh251@gmail.com>
2026-03-23 16:39:00 +0000

    Mon, Mar 23, 2026, 9:39 AM -07:00

--- script/insect.js
+++ script/insect.js
@@ -26,10 +26,30 @@ function startGame() {
 
 
 function createInsect() {
-    insect = document.createElement('div')
+    console.log("hello")
+    insenct = document.createElement('div')
     insect.classList.add('insect')
-    insect.innerHtml = 
-    insect.innerHTML = "<img src= "images/roachng.png" alt="roach"
+    {x,y}= getRandomLocation()
+    insect.style.top = '$(y)px'
+    insect.style.left = '$(x)px'
+    insect.inner.HTML = '<img src="${src}" alt="${src}" style= "transform: rotate(${Math.random()*})
+    game_container.appendChild(insect)
 
 
+}
+
+function getRandomLocation() {
+    width= window.innerWidth
+    height = window.innerHeight
+    x=Math.random()*(width -200) + 100
+    y=Math.random()*(height-200) + 100
+    return(x,y)
+
+}
+
+function catchInsect(){
+    this.classList.add('caught')
+    this.remove(), 2000
+    setTimeout( () => this.remove(), 2000)
+    addInsects()
 }
