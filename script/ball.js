85bd1547d77c0c993b0e35541bf99c6997e98fb8
AshbirSingh <ashbirsingh251@gmail.com>
2026-04-13 16:17:18 +0000

    Mon, Apr 13, 2026, 9:17 AM -07:00

--- script/ball.js
+++ script/ball.js
@@ -2,13 +2,15 @@ const ball = document.createElement('div')
 const ballRadius = 30
 const windowHeight = window.innerHeight
 const windowWidth = window.innerWidth
-let ballXPoisition = windowWidth/2 - ballRadius
+let ballXPosition = windowWidth/2 - ballRadius
+let ballSpeed = 5
 
 setInterval(moveBall, 10)
 
 function moveBall(){
-    ballXPosition = ballXPosition + 5
+    ballXPosition = ballXPosition + ballSpeed
     ball.style.left = `${ballXPosition}px`
+    if (ballXPosition)
 }
 
 
