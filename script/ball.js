0072b95bfaa7a0250b424af8289673ff977a001f
AshbirSingh <ashbirsingh251@gmail.com>
2026-04-13 15:59:56 +0000

    Mon, Apr 13, 2026, 8:59 AM -07:00

--- script/ball.js
+++ script/ball.js
@@ -1,7 +1,11 @@
-
+const ball = document.createElement('div')
 
 function createBall(){
     document.body.appendChild(ball)
     ball.style.height = "100px"
-    ball.style
+    ball.style.width = "100px"
+    ball.style.borderRadius = "50%"
+    ball.style.backgroundColor = "green"
+    ball.style.position = "absolute"
+    
 }
