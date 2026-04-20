37dd2d896409fb3c3e8fbd4f8b5c50897d323334
AshbirSingh <ashbirsingh251@gmail.com>
2026-04-20 06:48:45 +0000

    Sun, Apr 19, 2026, 11:48 PM -07:00

---
+++ relax.js
@@ -0,0 +1,44 @@
+const container = document.getElementById('container')
+const text = document.getElementById('text')
+
+
+const totalTime = 7500
+const breathTime = (totalTime / 5) * 2
+const holdTime = totalTime / 5
+
+
+breathAnimation()
+
+
+function breathAnimation() {
+   text.innerText = 'Breathe In!'
+
+
+   container.classList.remove('shrink')
+   container.classList.remove('grow')
+   void container.offsetWidth
+   container.classList.add('grow')
+
+
+   setTimeout(() => {
+       text.innerText = 'Hold'
+
+
+       setTimeout(() => {
+           text.innerText = 'Breathe Out!'
+
+
+           container.classList.remove('grow')
+           container.classList.remove('shrink')
+           void container.offsetWidth
+           container.classList.add('shrink')
+
+
+       }, holdTime)
+
+
+   }, breathTime)
+}
+
+
+setInterval(breathAnimation, totalTime)
